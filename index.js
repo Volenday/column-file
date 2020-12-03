763af1b6c2ed247d4c14f0159d2d77dc6bb87c2e
josua <josua.lagat@volenday.com>
2020-12-03 15:46:38 +0800

    update dependencies and added browser

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Image from './image';
 
+const browser = typeof process.browser !== 'undefined' ? process.browser : true;
+
 export default ({ fixedWidth, multiple, width, ...defaultProps }) => {
 	return {
 		...defaultProps,
 		disableFilters: true,
 		width: fixedWidth ? width : 100,
 		Cell: ({ value }) => {
-			return <Image multiple={multiple} value={value} />;
+			return browser ? <Image multiple={multiple} value={value} /> : null;
 		}
 	};
 };
